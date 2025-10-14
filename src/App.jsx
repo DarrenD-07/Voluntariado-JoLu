bcb6ef921d6a3e28a04fe24d6fedbb8e54ae59f3
Sulee20 <3059817810301@ingenieria.usac.edu.gt>
2025-10-14 17:22:45 -0600

    Completada pestana contactos y quienes somos, falta cambiar foto

--- src/App.jsx
+++ src/App.jsx
@@ -82,24 +82,12 @@ function App() {
                 </div>
               </div>
               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
-              </p>
-              <p>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam
-                rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-                explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
-                consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-              </p>
-              <p>
-                Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia
-                non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad
-                minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea
-                commodi consequatur?
-              </p>
+              Jolu de Guatemala ONG, fue creada con el fin de apoyar a adolescentes y jóvenes, comunidad LGBTIQ+, salud mental, e impactar en la educación de Guatemala, promoviendo la igualdad y los derechos humanos.
+            </p>
+            <p>
+              La organización nace en el año 2016, con la idea de apoyar a adolescentes y jóvenes, en temas de sexualidad, salud sexual y reproductiva, derechos sexuales y derechos reproductivos, prevención de embarazos no planificados, prevención de infecciones de transmisión sexual, prevención del VIH/SIDA, entre otros temas.
+En esa línea de ideas, se desarrollan actividades públicas informativas, que permitan comunicar a las poblaciones, en las diferentes temáticas de interés; estas actividades nos permiten dar orientación y asesoría de manera personalizada, según las necesidades de la población.
+            </p>
             </div>
             <div className="decorative-circles">
               <div className="circle"></div>
